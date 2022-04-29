fe9d38243f62749da05f860bda45bf5b43030b79
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2022-04-29 05:23:10 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JPWN5AwihGDgzXA-di1Eel8iBf3c0sI_LOWLV4-nZlk/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1JPWN5AwihGDgzXA-di1Eel8iBf3c0sI_LOWLV4-nZlk/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBRBVqGWH7Ibcr30SpLU-J49N9BCg_6uqE';
